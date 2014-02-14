6a25ca199e515bfb09933f6fdd0c5b1e4725016a
fisted <van.fisted@googlemail.com>
2014-02-14 01:56:07 +0100

    add peerlist module (stub)

---
+++ src/peerlist.c
@@ -0,0 +1,11 @@
+/* peerlist.c - (C) 2014, Timo Buhrmester
+ * srsotr - none of your business
+ * See README for contact-, COPYING for license information. */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "peerlist.h"
+
+
---
+++ src/peerlist.h
@@ -0,0 +1,8 @@
+/* peerlist.h - (C) 2014, Timo Buhrmester
+ * srsotr - none of your business
+ * See README for contact-, COPYING for license information. */
+
+#ifndef PEERLIST_H
+#define PEERLIST_H 1
+
+#endif /* PEERLIST_H */
